09989c2ce03a821623f8c20d285d56555bf120d1
VladimirSherban <Vovan_Sh_94@mail.ru>
2022-12-20 16:05:16 +0300

    Homework done!

---
+++ src/com/Skypro/Author.java
@@ -0,0 +1,24 @@
+package com.Skypro;
+
+public class Author {
+    String firstName;
+    String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
